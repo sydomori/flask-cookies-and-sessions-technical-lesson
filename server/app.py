92c7e55de004346f3948a0714d924b0f6fa3fb56
sydomori <sydneyomori@gmail.com>
2026-07-03 21:44:10 +0300

    create logic

--- server/app.py
+++ server/app.py
@@ -8,6 +8,27 @@ app.json.compact = False
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
 # Build Routes
+@app.route("/sessions/<string:key>", methods=['GET'])
+def show_session(key):
+    session['hello'] = session.get('hello') or 'World'
+    session['goodnight'] = session.get('goodnight') or 'Moon'
+    session['count'] = session.get('count') or 0
+
+    if key == 'count':
+        session['count'] += 1
+
+    response = make_response(jsonify({
+        'session': {
+            'session_key': key,
+            'session_value': session[key],
+            'session_accessed': session.accessed,
+        },
+        'cookies': [{cookie: request.cookies[cookie]}
+            for cookie in request.cookies],
+    }), 200)
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
